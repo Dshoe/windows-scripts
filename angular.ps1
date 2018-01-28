fa0579a5a044e72c5421da2fdf0fa61831f9a727
Devin Shoemaker <devinshoe@gmail.com>
2018-01-28 14:33:36 -0600

    angular.ps1: Install Angular and clone source code

---
+++ angular.ps1
@@ -0,0 +1,11 @@
+# Windows - Install Angular and Clone Source Code
+
+# Install Angular
+npm i -g @angular/cli
+
+# Create Source Code Directory
+mkdir %userprofile%/code/angular
+cd %userprofile%/code/angular
+
+# Clone Source Code
+git clone https://github.com/Dshoe/angular-tour-of-heroes
