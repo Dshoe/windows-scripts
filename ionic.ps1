9f431487494cad3ee3cac5174e460bfb14d83869
Devin Shoemaker <devinshoe@gmail.com>
2018-01-28 14:33:51 -0600

    ionic.ps1: Install Ionic and clone source code

---
+++ ionic.ps1
@@ -0,0 +1,14 @@
+# Windows - Install Ionic and Clone Source Code
+
+# Install Ionic
+npm i -g ionic cordova
+
+# Create Source Code Directory
+mkdir %userprofile%/code/ionic
+cd %userprofile%/code/ionic
+
+# Clone Source Code
+git clone https://github.com/dshoe/taskit-ionic.git
+git clone https://github.com/dshoe/CryptoProfitLog.git
+git clone https://github.com/dshoe/DaysSince.git
+git clone https://github.com/dshoe/WaterWatch.git
