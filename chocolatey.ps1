406c8df121ccf3ee287e4eb1f01c6140087e02d0
Devin Shoemaker <devinshoe@gmail.com>
2018-01-28 03:31:06 -0600

    chocolatey.ps1: Install Chocolatey and packages

---
+++ chocolatey.ps1
@@ -0,0 +1,7 @@
+# Windows - Install Chocolatey and Packages
+
+# Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Install Packages
+choco install -y AndroidStudio cmder etcher git gitkraken googlechrome intellijidea-ultimate jdk8 jre8 nodejs sudo transmission visualstudiocode
