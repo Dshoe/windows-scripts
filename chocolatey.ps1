742fa99fd56b609cf9b175b1d2f2492a84c0bd9a
Devin Shoemaker <devinshoe@gmail.com>
2018-02-04 09:40:41 -0600

    Install NVM instead of Node.js

--- chocolatey.ps1
+++ chocolatey.ps1
@@ -4,4 +4,4 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Install Packages
-choco install -y AndroidStudio cmder etcher git gitkraken googlechrome intellijidea-ultimate jdk8 jre8 nodejs sudo transmission visualstudiocode
+choco install -y AndroidStudio cmder etcher git gitkraken googlechrome intellijidea-ultimate jdk8 jre8 nvm sudo transmission visualstudiocode vlc
