8ffd16eec1c2d58fc2ba96d9a9d5b96186084aeb
Devin Shoemaker <devinshoe@gmail.com>
2018-02-22 16:16:20 -0600

    Create separate `ionic-projects.ps1` script

---
+++ ionic-projects.ps1
@@ -0,0 +1,10 @@
+# Windows - Clone Ionic Projects
+
+mkdir -p ~/code/ionic
+cd ~/code/ionic
+
+# Clone Source Code
+git clone https://github.com/dshoe/taskit-ionic.git
+git clone https://github.com/dshoe/CryptoProfitLog.git
+git clone https://github.com/dshoe/DaysSince.git
+git clone https://github.com/dshoe/WaterWatch.git
--- ionic.ps1
+++ ionic.ps1
@@ -2,13 +2,3 @@
 
 # Install Ionic
 npm install -g ionic cordova
-
-# Create Source Code Directory
-mkdir %userprofile%/code/ionic
-cd %userprofile%/code/ionic
-
-# Clone Source Code
-git clone https://github.com/dshoe/taskit-ionic.git
-git clone https://github.com/dshoe/CryptoProfitLog.git
-git clone https://github.com/dshoe/DaysSince.git
-git clone https://github.com/dshoe/WaterWatch.git
