668e75767a15c1edde72299c329b22b4ee32ab79
Devin Shoemaker <devinshoe@gmail.com>
2018-07-19 14:52:31 -0700

    chocolately: install Steam

--- chocolatey.ps1
+++ chocolatey.ps1
@@ -4,4 +4,4 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Install Packages
-choco install -y AndroidStudio cmder etcher epicgameslauncher firefox git gitkraken googlechrome intellijidea-ultimate jdk8 jre8 nvm slack sudo telegram transmission visualstudiocode vlc
+choco install -y AndroidStudio cmder etcher epicgameslauncher firefox git gitkraken googlechrome intellijidea-ultimate jdk8 jre8 nvm slack steam sudo telegram transmission visualstudiocode vlc
